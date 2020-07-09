74b1d2d8fde63d2d26ca13502cca6c8de96f0064
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-07-09 22:39:13 +0700

    Initial commit for the demo

---
+++ src/main/java/com/ps/demohikaricpconfiguration/DemoHikaricpConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.ps.demohikaricpconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHikaricpConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHikaricpConfigurationApplication.class, args);
+    }
+
+}
